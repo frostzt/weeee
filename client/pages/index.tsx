97095a9cf3bb53a716b95b844418c5652896884a
frostzt <aidenfrostbite@gmail.com>
2021-05-25 15:25:09 +0530

    nextjs initial commit

---
+++ client/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Weeee - Management made easier</title>
+        <meta
+          name="description"
+          content="A simple application to manage your team, projects, and tasks."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
