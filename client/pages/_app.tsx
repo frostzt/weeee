e1a7a936c56aacb5ae073f2a178d817a987da02e
frostzt <aidenfrostbite@gmail.com>
2021-05-26 16:52:30 +0530

    Working on landing page: included fonts (Lato)

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,20 @@
 import "../styles/globals.scss";
+import Head from "next/head";
 import type { AppProps } from "next/app";
+import { Fragment } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 export default MyApp;
