225d90acfa2b2b2ca0e71e556832b1d21ee30df4
frostzt <aidenfrostbite@gmail.com>
2021-05-29 10:43:45 +0530

    Working on landing page: Features section ready, popup ready and popup animation ready

--- frontend/components/FeaturePopup/FeaturePopup.tsx
+++ frontend/components/FeaturePopup/FeaturePopup.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 
 // Styles
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import styles from "./FeaturePopup.module.scss";
 
 interface Props {
@@ -12,15 +12,34 @@ interface Props {
 }
 
 const FeaturePopup: React.FC<Props> = ({ color, handler, heroUrl }) => {
+  const backdropControl = useAnimation();
+  const imageControl = useAnimation();
+  const contentControl = useAnimation();
+
+  const sequence = async () => {
+    await backdropControl.start({ opacity: 1 }, { duration: 0.3 });
+    await imageControl.start({ x: 0, opacity: 1 }, { duration: 0.3 });
+    return await contentControl.start({ x: 0, opacity: 1 }, { duration: 0.3 });
+  };
+
+  useEffect(() => {
+    sequence();
+  }, []);
+
   return (
     <Fragment>
       <motion.div className={styles.container}>
-        <motion.div className={styles.imageContainer} style={{ backgroundColor: color }}>
-          <Image className={styles.imageContainer__image} src={heroUrl} layout="fill" />
+        <motion.div
+          initial={{ x: 200, opacity: 0 }}
+          animate={imageControl}
+          className={styles.imageContainer}
+          style={{ backgroundColor: color }}
+        >
+          <Image priority className={styles.imageContainer__image} src={heroUrl} layout="fill" />
         </motion.div>
-        <motion.div className={styles.content}></motion.div>
+        <motion.div initial={{ x: -200, opacity: 0 }} animate={contentControl} className={styles.content}></motion.div>
       </motion.div>
-      <motion.div onClick={handler} className={styles.backdrop} />
+      <motion.div initial={{ opacity: 0 }} animate={backdropControl} onClick={handler} className={styles.backdrop} />
     </Fragment>
   );
 };
