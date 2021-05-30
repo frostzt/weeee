16178daf554c0a580c3424be1729a8e70447be7d
frostzt <aidenfrostbite@gmail.com>
2021-05-30 20:45:50 +0530

    Working on landing page: Features section ready, animating the entire section

--- frontend/components/Button/Button.tsx
+++ frontend/components/Button/Button.tsx
@@ -14,6 +14,9 @@ const Button: React.FC<Props> = ({ children, extraClass }) => {
       whileHover={{
         scale: "1.1",
       }}
+      whileTap={{
+        scale: "0.9",
+      }}
       className={`${styles.button} ${extraClass ? extraClass : ""}`}
     >
       {children}
