ca563ffe6f7619a9f4f53bb283da15b1786d7c14
frostzt <aidenfrostbite@gmail.com>
2021-06-18 19:04:55 +0530

    Working on the about page

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -9,6 +9,31 @@ const AboutPage = () => {
       <h1 className={styles.title}>
         What is <span className={styles.highlight}>Weeee?</span>
       </h1>
+      <div className={styles.content}>
+        Weeee is a platform where you can manage, create, and do everything with
+        your projects, teams, and even your own organization!
+        <br />
+        <br />
+        Yep that was the one-liner! Weeee is more of a personal side-project
+        that I started, however I kept it open-source and made it sort of an
+        application so that people can use it freely and even develop on top of
+        it!
+        <br />
+        <br />
+        It consists of everything that a team or an organization might need to
+        manage things on their own. You don't have to purchase any subscription
+        or even any software for that, weeee are here for you! This is quite
+        easy to deploy and use, best part of weeee is that it is made for
+        developers so that they can customize it with whatever feature they
+        want!
+        <br />
+        <br />
+        So in a nutshell this thing that you're seeing here is not a application
+        that you can use, you will need to deploy it and then it is ready for
+        production! Now don't get me wrong the reason why I haven't deployed it
+        is because the costs would get too much and I don't really think I would
+        be able to handle it out!
+      </div>
     </div>
   );
 };
