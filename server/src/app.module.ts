4cf5ca99a445a16af66f0cf8dea2ea2bc82b08a7
frostzt <aidenfrostbite@gmail.com>
2021-07-16 14:07:30 +0530

    implementing users and auth

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -9,6 +11,8 @@ import { AppService } from './app.service';
       debug: true,
       playground: true,
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
