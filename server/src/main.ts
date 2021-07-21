cf5ef8ab504f0e673a69db63dfcc4e17b2c5a0b0
frostzt <aidenfrostbite@gmail.com>
2021-07-21 11:15:09 +0530

    created custom GqlAuthGuard and CurrentUser decorator; implemented authentication using passport-jwt

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3000;
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('APP_PORT') || 3000;
 
   // Global Pipes
   app.useGlobalPipes(new ValidationPipe());
--- server/src/users/users.resolver.ts
+++ server/src/users/users.resolver.ts
@@ -1,6 +1,4 @@
-import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { AuthGuard } from '@nestjs/passport';
 import { CreateUserInput } from './inputs/create-user.input';
 import { LoginUserInput } from './inputs/login-user.input';
 import { UsersService } from './users.service';
@@ -13,12 +11,6 @@ export class UsersResolver {
   @Query(() => String)
   signIn(@Args('loginData') loginData: LoginUserInput) {
     return this.usersService.signIn(loginData);
-  }
-
-  @Query(() => String)
-  @UseGuards(AuthGuard())
-  test() {
-    return 'success';
   }
 
   // Mutations
