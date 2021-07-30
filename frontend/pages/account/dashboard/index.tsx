78f270a785778303e67c7b903c6dcf70d237e759
frostzt <aidenfrostbite@gmail.com>
2021-07-30 20:42:57 +0530

    creating a route protecting HOC

--- frontend/pages/account/dashboard/index.tsx
+++ frontend/pages/account/dashboard/index.tsx
@@ -1,4 +1,5 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import { decode } from 'jsonwebtoken';
 
 import { GetServerSideProps } from 'next';
 import styles from './dashboard.module.scss';
@@ -7,8 +8,38 @@ import styles from './dashboard.module.scss';
 import AuthContext from '../../../contexts/AuthContext/Auth.context';
 import { Button } from '../../../components/Button/Button';
 
-const DashboardPage = (props: any) => {
-  const { signOut } = useContext(AuthContext);
+interface PageProps {
+  email: string;
+}
+
+const DashboardPage: React.FC<PageProps> = ({ email }) => {
+  const [loading, setLoading] = useState<Boolean>(true);
+  const [isAuthenticated, setIsAuthenticated] = useState<Boolean>(false);
+
+  // Context
+  const { signOut, user }: { signOut: any; user: any } =
+    useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      if (user!.email === email) {
+        setIsAuthenticated(true);
+      }
+    }
+    setLoading(false);
+  }, [user]);
+
+  // If loading
+  if (loading) {
+    return <h1 style={{ color: 'white' }}>Loading...</h1>;
+  }
+
+  // If not authenticated
+  if (!isAuthenticated) {
+    console.log(user);
+    console.log(email);
+    return <h1 style={{ color: 'white' }}>Not verified! Logged!</h1>;
+  }
 
   return (
     <h1>
@@ -20,22 +51,21 @@ const DashboardPage = (props: any) => {
 export default DashboardPage;
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  let email = null;
+
   if (req.headers.cookie) {
     const tokens = req.headers.cookie.split(';');
     const token = tokens.find((token) => token.includes('accessToken'));
     if (token) {
-      console.log('Access token found!');
       const accessToken = token.trim().split('=')[1];
-      console.log(accessToken);
+      const decodedToken = decode(accessToken, { complete: true });
+      email = decodedToken?.payload.email;
     }
   }
 
-  console.log('All available cookies!');
-  console.log(req.headers.cookie);
-
   return {
     props: {
-      boi: 'Hello',
+      email,
     },
   };
 };
