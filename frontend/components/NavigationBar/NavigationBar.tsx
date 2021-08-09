5852202a8c362afaf1e98f3bf9f786d787e50332
frostzt <aidenfrostbite@gmail.com>
2021-08-09 18:41:06 +0530

    implementing navigation bar for authenticated users

---
+++ frontend/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,8 @@
+// Styling
+import styles from './NavigationBar.module.scss';
+
+const NavigationBar = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default NavigationBar;
