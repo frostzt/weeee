a63e5b33964c69d53029ae1c8b1f85d136a9e0a5
frostzt <aidenfrostbite@gmail.com>
2021-08-10 13:55:02 +0530

    implemented NavigationBar funcionality

--- frontend/components/NavigationBar/NavigationBar.tsx
+++ frontend/components/NavigationBar/NavigationBar.tsx
@@ -1,21 +1,24 @@
+import Link from 'next/link';
 import React, { useContext, useState } from 'react';
-import NavigationBarContext from '../../contexts/NavigationBar/NavigationBar.context';
+import NavigationBarContext, { AvailablePages } from '../../contexts/NavigationBar/NavigationBar.context';
 
 // Varients
 import { containerVarient, optionVarient } from './NavigationBar.varients';
 
 // Styling
 import cx from 'classnames';
+import { FaUser } from 'react-icons/fa';
 import { MdArrowDropUp } from 'react-icons/md';
+import { AiFillDashboard } from 'react-icons/ai';
 import styles from './NavigationBar.module.scss';
 import { AnimatePresence, motion } from 'framer-motion';
 
 const NavigationBar: React.FC = () => {
+  // States
   const [isOpen, setIsOpen] = useState(false);
 
+  // Navigation Bar Context
   const { current } = useContext(NavigationBarContext);
-
-  console.log(current);
 
   return (
     <motion.div variants={containerVarient} initial="initial" animate="animated" className={styles.container}>
@@ -26,7 +29,24 @@ const NavigationBar: React.FC = () => {
         />
         <AnimatePresence>
           {isOpen && (
-            <motion.div variants={optionVarient} initial="initial" animate="animated" exit="exit" className={styles.options}></motion.div>
+            <motion.div variants={optionVarient} initial="initial" animate="animated" exit="exit" className={styles.options}>
+              <div className={styles.links}>
+                <div className={styles.links__link}>
+                  <Link href="/account/dashboard">
+                    <div className={cx([styles.links__link_icon, current === AvailablePages.Dashboard ? styles.selected : null])}>
+                      <AiFillDashboard title="Dashboard" />
+                    </div>
+                  </Link>
+                </div>
+                <div className={styles.links__link}>
+                  <Link href="/account/me">
+                    <div className={cx([styles.links__link_icon, current === AvailablePages.Me ? styles.selected : null])}>
+                      <FaUser title="My Profile" />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </motion.div>
           )}
         </AnimatePresence>
       </div>
