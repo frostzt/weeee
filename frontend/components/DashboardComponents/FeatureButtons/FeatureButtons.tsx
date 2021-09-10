ebebcf4fb1eef6b939f38794bd0b369c357e6659
frostzt <aidenfrostbite@gmail.com>
2021-09-10 20:03:11 +0530

    working on CompanyAnnouncements on Backend

--- frontend/components/DashboardComponents/FeatureButtons/FeatureButtons.tsx
+++ frontend/components/DashboardComponents/FeatureButtons/FeatureButtons.tsx
@@ -7,11 +7,12 @@ import styles from './FeatureButtons.module.scss';
 interface Props {
   Icon: IconType;
   title: string;
+  handler: () => void;
 }
 
-const FeatureButtons: React.FC<Props> = ({ Icon, title }) => {
+const FeatureButtons: React.FC<Props> = ({ Icon, title, handler }) => {
   return (
-    <div className={styles.container}>
+    <div onClick={handler} className={styles.container}>
       <h3 className={styles.title}>{title}</h3>
       <div className={styles.icon}>
         <Icon className={styles.icon__inner} />
--- frontend/pages/account/dashboard/index.tsx
+++ frontend/pages/account/dashboard/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
-import { Fragment, useContext, useEffect } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 
 // Styles
-import { BsFillMicFill } from 'react-icons/bs';
 import { BiTask } from 'react-icons/bi';
+import { BsFillMicFill } from 'react-icons/bs';
 import styles from './dashboard.module.scss';
 
 // Components, Interfaces, HOCs
@@ -12,6 +12,7 @@ import { FullUser } from '../../../interfaces/User.interface';
 import { withLoading } from '../../../HOC/withLoading/withLoading';
 import FeatureButtons from 'components/DashboardComponents/FeatureButtons/FeatureButtons';
 import { requireAuthentication } from '../../../HOC/requireAuthentication/requireAuthentication';
+import CompanyAnnouncements from 'components/DashboardComponents/CompanyAnnouncements/CompanyAnnouncements';
 import NavigationBarContext, { AvailablePages } from '../../../contexts/NavigationBar/NavigationBar.context';
 
 interface PageProps {
@@ -19,6 +20,9 @@ interface PageProps {
 }
 
 const DashboardPage: React.FC<PageProps> = ({ user }) => {
+  const [showTasks, setShowTasks] = useState<Boolean>(false);
+  const [showAnnouncement, setShowAnnouncement] = useState<Boolean>(false);
+
   // Globally let the NavBar know that page change occured
   const { changePage } = useContext(NavigationBarContext);
   useEffect(() => {
@@ -27,17 +31,28 @@ const DashboardPage: React.FC<PageProps> = ({ user }) => {
 
   console.log(user);
 
+  // Handlers
+  const handleShowAnnouncements = () => {
+    setShowAnnouncement((prevState) => !prevState);
+  };
+
+  const handleShowTasks = () => {
+    setShowTasks((prevState) => !prevState);
+  };
+
   return (
     <Fragment>
+      {showAnnouncement && <CompanyAnnouncements user={user} />}
       <Head>
         <title>Dashboard - Weeee</title>
         <meta name="description" content="Weeee dashboard, manage everything at one place!" />
       </Head>
       <div className={styles.container}>
         <div className={styles.header}>Dashboard</div>
+        <h2 className={styles.title}>Quick view</h2>
         <div className={styles.features}>
-          <FeatureButtons title="Announcements" Icon={BsFillMicFill} />
-          <FeatureButtons title="Tasks" Icon={BiTask} />
+          <FeatureButtons title="Announcements" Icon={BsFillMicFill} handler={handleShowAnnouncements} />
+          <FeatureButtons title="Tasks" Icon={BiTask} handler={handleShowTasks} />
         </div>
       </div>
     </Fragment>
