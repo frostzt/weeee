bc57b96dc69faa1962f2594fcd346dbf016db02a
frostzt <aidenfrostbite@gmail.com>
2021-12-06 16:25:53 +0530

    upated: fe with envs

--- frontend/Config/Config.ts
+++ frontend/Config/Config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
